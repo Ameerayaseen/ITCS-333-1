b1bad83bd34426f3d9d9273a634915fe21675880
ZainabAbdulKhudhur <zooza029zz@gmail.com>
2025-05-14 21:46:30 +0300

    Update news.php

--- phase3/news.php
+++ phase3/news.php
@@ -1,6 +1,10 @@
 <?php
 //Connection to database
-header("Content-Type: application/json");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
 // DB prarmeters
 $require_once 'db.php';  
 
