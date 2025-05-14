bbff4d609edae1057c26163c7b06220e4f70a95c
ZahraAbdulkhudhur <z.hsanbh09@gmail.com>
2025-05-14 21:51:23 +0300

    Update ClubActivities.php

--- phase3/ClubActivities.php
+++ phase3/ClubActivities.php
@@ -1,6 +1,11 @@
 
 <?php
 // Connection to database
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
 header("Content-Type: application/json"); 
 
 $require_once 'db.php';
