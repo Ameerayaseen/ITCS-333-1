747ca16f77fde404291ae4ffac01f0f90450c9a9
Zahraa028 <202005540@stu.uob.edu.bh>
2025-05-14 21:00:41 +0300

    Update db.php

--- phase3/db.php
+++ phase3/db.php
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+  	<title>PHP + MySQL</title>
+  	<meta charset="UTF-8">
+  </head>
+  <body>
 <?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=zahraa;charset=utf8', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $ex) {
-    echo json_encode(["error" => "Database connection failed: " . $ex->getMessage()]);
-    exit;
-}
+  $host = "127.0.0.1";
+  $user = getenv("userzahraa");
+  $pass = getenv("3732288");
+  $db = getenv("zahraa");
+
+  $conn = new mysqli($host, $user, $pass, $db);
+  if ($conn->connect_error)
+      die("Connection failed: " . $conn->connect_error);
+  echo "<p>DATABASE connected succesfully!</p>";
+  $conn->close();
 ?>
+  </body>
+</html>
