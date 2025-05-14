fffbad418e9b1ce0351e75486c01e395f7214dda
Zahraa028 <202005540@stu.uob.edu.bh>
2025-05-14 15:32:10 +0300

    Update event.php

--- phase3/event.php
+++ phase3/event.php
@@ -4,13 +4,13 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
-$host = 'localhost';
-$db   = 'campus_hub';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+$require_once 'db.php';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
