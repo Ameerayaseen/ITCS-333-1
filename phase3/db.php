ab816075a7d56b4c6273852d4a41b5666d28ddaa
Zahraa028 <202005540@stu.uob.edu.bh>
2025-05-14 21:12:44 +0300

    Update db.php

--- phase3/db.php
+++ phase3/db.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en-US">
-  <head>
-  	<title>PHP + MySQL</title>
-  	<meta charset="UTF-8">
-  </head>
-  <body>
-<?php
-  $host = "127.0.0.1";
-  $user = getenv("userzahraa");
-  $pass = getenv("3732288");
-  $db = getenv("zahraa");
 
-  $conn = new mysqli($host, $user, $pass, $db);
-  if ($conn->connect_error)
-      die("Connection failed: " . $conn->connect_error);
-  echo "<p>DATABASE connected succesfully!</p>";
-  $conn->close();
+<?php
+$host = "127.0.0.1";
+$db = "zahraa";        
+$user = "userzahraa";      
+$pass = "3732288";  
+$charset = "utf8mb4";
 ?>
-  </body>
-</html>
