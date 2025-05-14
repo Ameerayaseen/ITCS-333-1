911700d25c7f854cf71f84990e67f758df962fb4
ZahraAbdulkhudhur <z.hsanbh09@gmail.com>
2025-05-14 21:49:43 +0300

    updatecode

--- phase3/ClubActivities.php
+++ phase3/ClubActivities.php
@@ -1,5 +1,6 @@
 <?php
 // Connection to database
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
